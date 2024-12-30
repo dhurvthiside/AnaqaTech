8c76d3c8ff0cc3404ff3c632f8ef0f6a0d27ce8a
dhurvthiside <dhruvchauhan6488@gmail.com>
2024-12-31 01:42:10 +0530

    change4

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { HashRouter } from 'react-router-dom'; // Import HashRouter instead of BrowserRouter
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-      <App />
-  </BrowserRouter>
-)
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
